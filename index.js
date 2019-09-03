7747feb90abeeb7ab4ea0ff1e9e267886ac6c2ef
joaocosti <joao_costi@hotmail.com>
2019-09-03 19:55:33 -0300

    no message

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 
 const bodyParser = require('body-parser')
 
-const path = require('path')
+const patho = require('path')
 
 const sqlite = require('sqlite');
-const dbConnection = sqlite.open(path.resolve(__dirname,'banco.sqlite') , { Promise });
+const dbConnection = sqlite.open(patho.resolve(__dirname,'banco.sqlite') , { Promise });
 
 const port = process.env.PORT || 3000
 
